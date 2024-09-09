48516543da28ac36e4db5b869914bd0172a493d0
abdul-zacky <zacxvan@gmail.com>
2024-09-09 13:50:45 +0700

    fourth last try

--- storee/settings.py
+++ storee/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=nu8%$5-qe-cwi7q#vea4eu)8o!_)o1wu!lic0(o8t#39r^&2t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "abdul-zacky-storee.pbp.cs.ui.ac.id"]
 
