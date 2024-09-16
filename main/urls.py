64df8dbb0cbe04f98a565ce081c52039efe82cb9
abdul-zacky <zacxvan@gmail.com>
2024-09-16 18:55:42 +0700

    Bikin form

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from main.views import show_main
-# from django.urls import path, include
+from main.views import show_main, create_product
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-    # path('', include('main.urls')),
+    path('create-product', create_product, name='create_product'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,10 +4,23 @@ from main.models import Product
 
 # Create your views here.
 def show_main(request):
+    products = Product.objects.all()
+
     context = {
         'name' : 'Abdul Zacky',
         'npm': '2306214510',
-        'class': 'PBP C'
+        'class': 'PBP C',
+        'new product': products,
     }
 
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
