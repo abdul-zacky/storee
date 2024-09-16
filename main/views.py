5e0b3a63dfdceb76354a417af2ace59a2691f6e8
abdul-zacky <zacxvan@gmail.com>
2024-09-16 20:32:06 +0700

    show xml json by id

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect   # Tambahkan import redirect di baris ini
 from main.forms import ProductForm
 from main.models import Product
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_main(request):
@@ -10,7 +12,7 @@ def show_main(request):
         'name' : 'Abdul Zacky',
         'npm': '2306214510',
         'class': 'PBP C',
-        'new product': products,
+        'products': products,
     }
 
     return render(request, "main.html", context)
@@ -24,3 +26,19 @@ def create_product(request):
 
     context = {'form': form}
     return render(request, "create_product.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
